bc31bbb8b2d0712797200b61ad7161e4fb07a949
Austin Perry <austinjperry22@gmail.com>
2024-04-29 16:35:48 -0600

    changed docker files

--- client/src/env.js
+++ client/src/env.js
@@ -3,9 +3,9 @@ export const baseURL = dev ? 'http://localhost:3000' : ''
 
 export const spotifyUrl = 'https://api.spotify.com'
 export const useSockets = false
-export const domain = 'dev-r2l4kl7ifqatct23.us.auth0.com'
-export const clientId = '3ugFmbTisZI8MvM7ux0qRbSLZS8X8Zo1'
+export const domain = 'dev-lucu1eq62l0de56p.us.auth0.com'
+export const clientId = 'Pm5TQLhJkButJ3ZIrllfYVfdTz1CRJbD'
 
 export const spotifyClientId = 'f60d41d8f71b48e59709254f06a045e8'
 export const redirectUri = 'http://localhost:8080/#/application'
-export const audience = 'https://tylerApi.com'
+export const audience = 'https://codeworksAPI.com'
